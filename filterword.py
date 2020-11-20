8445fa594d9904fc93cd4c5c72cc963c808666af
discord_cyi <70206738+cirunocyi@users.noreply.github.com>
2020-11-20 22:08:41 +0900

    Delete filterword.py

--- filterword.py
+++
@@ -1,7 +0,0 @@
-your_list = ['나쁜말1','나쁜말2']
-
-if message.content.startswith(message.content):
-    if message.content in your_list:
-        await message.delete()
-
-# 필터링 할 말을 리스트에 지정해준 뒤, 4번째 라인  if message.content in your_list: 을 통해 메시지에 나쁜말 리스트중 하나가 포함되면 메시지를 지우는 원리
